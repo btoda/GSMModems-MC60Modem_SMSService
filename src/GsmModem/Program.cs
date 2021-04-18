3116a11909f423964182a1d1b14d00a758483403
Bogdan Toda <bogdan.toda@yahoo.ro>
2021-04-18 21:48:50 +0300

    Update gsm modem tester app to allow command line parameters for port name

--- src/GsmModem/Program.cs
+++ src/GsmModem/Program.cs
@@ -7,7 +7,11 @@ namespace GsmModem
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
-            GsmHandler handler = new GsmHandler("COM4", 9600, "0000");
+            string portName = "COM1";
+            if(args.Length>0){
+                portName = args[0];
+            }
+            GsmHandler handler = new GsmHandler(portName, 9600, "0000");
             handler.SendSms("0754588580"," TEST GSM MODEM ");
         }
     }
